1c187b593622c53c30801d9fc7b32a6ea18dd0fc
Li_Mengxun <mengxunli@whu.edu.cn>
2022-04-23 15:40:49 +0800

    2022-04-23 15:40:49.795920 @ Auto commit from monsoon-U59

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 file_path = [
         "~/.vimrc",
         "~/.zshrc",
+        "~/.antigenrc",
         "~/.tmux.conf",
         "~/.config/nvim/init.vim",
         "~/.config/nvim/coc-settings.json",
