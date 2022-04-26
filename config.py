4f14fb9fcfbfc6b25bcdc3deeddf5cd6b63aa3f1
Li_Mengxun <mengxunli@whu.edu.cn>
2022-04-26 15:51:15 +0800

    2022-04-26 15:51:15.210879 @ Auto commit from monsoon-U59

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 file_path = [
-        "~/.vimrc",
+        ".config/fish/config.fish",
         "~/.zshrc",
         "~/.antigenrc",
         "~/.tmux.conf",
+
         "~/.config/i3/config",
+        "~/.config/i3status/config",
+        "~/.config/compton.conf",
+
+        "~/.vimrc",
         "~/.config/nvim/init.vim",
         "~/.config/nvim/coc-settings.json",
         "~/init_machine/"
---
+++ files/init_machine/init_machine/initFolders.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+mkdir -p ~/Applet
+mkdir -p ~/Code
+mkdir -p ~/bin
+mkdir -p ~/Documents
+ln -s ~/Code ~/Documents/Code
