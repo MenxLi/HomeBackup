62fb77b5e70415958d0f3f90110cf030c69b292b
Li_Mengxun <mengxunli@whu.edu.cn>
2022-04-26 22:48:00 +0800

    2022-04-26 22:48:00.395679 @ Auto commit from monsoon-U59

--- files/init_machine/init_machine/initFolders.sh
+++ files/init_machine/init_machine/initFolders.sh
@@ -3,4 +3,14 @@ mkdir -p ~/Applet
 mkdir -p ~/Code
 mkdir -p ~/bin
 mkdir -p ~/Documents
-ln -s ~/Code ~/Documents/Code
+mkdir -p ~/Documents/Wallpapers
+cd ~/Documents/Wallpapers
+
+for (( c=0; c<=10; c++))
+do
+    wget http://limengxun.com/files/Wallpapers/$c.jpg
+done
+
+ln -s $HOME/Code ~/Documents/Code
+echo "Created file structures."
+
