fe980a5d7d982b7ea78f83fee914b7dd0408d0a7
li_mengxun <limengxun45@outlook.com>
2022-04-27 23:05:14 +0800

    2022-04-27 23:05:14.049740 @ Auto commit from tp-opensuse

--- files/init_machine/initFolders.sh
+++ files/init_machine/initFolders.sh
@@ -3,4 +3,11 @@ mkdir -p ~/Applet
 mkdir -p ~/Code
 mkdir -p ~/bin
 mkdir -p ~/Documents
+
+for ((c=0; c<=5; c++))
+do
+    wget http://limengxun.com/files/Wallpapers/$c.jpg
+done
+
 ln -s ~/Code ~/Documents/Code
+echo "initialized file structures"
